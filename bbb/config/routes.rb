b49e53c6e68d23aba1db5fc32d89fe86e3cf4cd8
EC2 Default User <ec2-user@ip-172-31-49-208.ec2.internal>
2020-12-16 04:53:39 +0000

    konn

--- bbb/config/routes.rb
+++ bbb/config/routes.rb
@@ -1,3 +1,13 @@
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users/index'
+  get 'users/new'
+  post 'users', to: 'users#create'
+  delete 'users/:id', to: 'user#destroy'
+  get 'top/main'
+  post 'top/login'
+  root 'top#main'
+  get 'top/logout'
+  resources :users, only: [:index, :new, :create, :destroy]
+  
 end
